b54d11552524cd0a5b5ee157af3f13884e060707
Justice <asibechetachukwu@gmail.com>
2025-10-26 04:21:20 +0000

    Password Restt

---
+++ apps/web/next.config.js
@@ -0,0 +1,37 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  env: {
+    SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
+    SENDGRID_FROM_EMAIL: process.env.SENDGRID_FROM_EMAIL,
+    SENDGRID_FROM_NAME: process.env.SENDGRID_FROM_NAME,
+  },
+  images: {
+    domains: ['aunxdbqukbxyyiusaeqi.supabase.co'],
+  },
+  async headers() {
+    return [
+      {
+        source: '/api/(.*)',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, POST, PUT, DELETE, OPTIONS',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'Content-Type, Authorization, X-Requested-With, x-authorization, x-auth-token, x-supabase-token',
+          },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
